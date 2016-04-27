ef882d9a41543d03e4ccb88092fcb52c61bbbfce
Brandon Curtis <brandon.curtis@gmail.com>
2016-04-27 08:13:48 -0700

    increased integration duration

--- bash/spectra.sh
+++ bash/spectra.sh
@@ -5,6 +5,5 @@ export LD_LIBRARY_PATH="$PWD/lib"
 cd ~/oceanoptics/seabreeze-3.0.11/SeaBreeze/sample-code/c
 
 while true; do
-	./seabreeze-util --get-spectrum --integration-time-us 1000000 > ~/repos/bcduino/data/2016-04-26/spectra/spectrum_$(date --iso-8601='s' | cut -d '-' -f 1-3 | sed 's/T0/_/')
-	sleep 1
+	./seabreeze-util --get-spectrum --integration-time-us 5000000 > ~/repos/bcduino/data/2016-04-26/spectra/spectrum_$(date --iso-8601='s' | cut -d '-' -f 1-3 | sed 's/T0/_/')
 done
