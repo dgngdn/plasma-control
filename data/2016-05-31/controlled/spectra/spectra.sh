2cd33d9c413603710fccf6666a5b7df1c9e67da3
Brandon Curtis <brandon.curtis@gmail.com>
2017-03-20 16:39:59 -0700

    preparing for sync

--- data/2016-05-31/controlled/spectra/spectra.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-oopath=/home/brandon/oceanoptics/seabreeze-3.0.11/SeaBreeze/
-export LD_LIBRARY_PATH=${oopath}lib
-export CWD=$1
-while true; do
-  ${oopath}/sample-code/c/seabreeze-util --get-spectrum --integration-time-us 1000000 > $1/spectrum_$(date --iso-8601='s' | cut -d '-' -f 1-3 | sed 's/T0/_/')
-  sleep 1
-done
